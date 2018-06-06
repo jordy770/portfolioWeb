be79450bf2eecd5ab58610d809ad821fb4b535ec
jordy770 <32267019+jordy770@users.noreply.github.com>
2018-06-06 10:31:18 +0200

    some changes

--- react/react-app/src/components/Home.jsx
+++ react/react-app/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import {Jumbotron, Grid, Row, Col, Image, Button, Carousel, CarouselItem, CarouselCaption} from 'react-bootstrap';
 import  './Home.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 
@@ -8,31 +8,64 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-      <Image src="assets/serenity_galaxy.1.jpg" className="home-header-image"/>
+      {/* <Image src="assets/serenity_galaxy.1.jpg" className="home-header-image"/> */}
         <Grid>
-          {/* <Jumbotron className="introtext-homepage">
-          </Jumbotron> */}
+          
           
 
            <Row className="show-grid text-center">
               <Col xs={12} sm={5} className="person-wrapper">
-              <ScrollAnimation animateIn='fadeIn'>
+              <ScrollAnimation animateIn='fadeIn' delay={1000}>
+              <Jumbotron className="introtext-homepage">
                 <h1>Welcome</h1>
                 <p> Dit is mijn portfolio website </p>
                 <p> Ik ben Jordy van Santen <br /> Ik ben een beginnend Creatieve Programmeur </p>
                 <p> Click Here if you want to know me Better!!</p>
-              </ScrollAnimation>
-              <ScrollAnimation delay={2000}animateIn="fadeIn">
+                <ScrollAnimation delay={2000}animateIn="fadeIn">
                 <Button className="home-button" bsSize="large" href="./About"> About ME! </Button>
                 <Button className="home-button" bsSize="large" href="./News"> My Projects! </Button>
               </ScrollAnimation>
+                </Jumbotron>
+              </ScrollAnimation>
               </Col>
               <Col xs={12} sm={7} className="person-wrapper">
-                <Image src="assets/mijn-foto.1.jpg" circle className="profile-pic"/>
-
+                <Image src="assets/.jpg" circle className="profile-pic"/>
               </Col>
             </Row>
+            <Row className="Aboutme-homepage">
+            <Col xs={12} sm={1} xsOffset={1}>
+              <h1> About me !! </h1>
+            </Col>
+            </Row>
+            <Row>
+              <Col xs={12} sm={1} xsOffset={1}>
+            <Carousel className="carousel-homepage">
+              <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src="assets/somepic.jpg" />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src="assets/somepic2.jpg" />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src="/assets/somepic3.jpg" />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>;
+            </Col>
+            </Row>
         </Grid>
+       
         </div>
     )
   }
